04c3b22fe4887859cb81fc68d965b112164b9e94
Jake Frischmann <jjfrisch@outlook.com>
2022-07-13 14:16:12 -0500

    nn

--- Runner.py
+++ Runner.py
@@ -1,13 +1,12 @@
-
+print("Extracting...")
 # import data
 import xlrd
- 
+
 # Give the location of the file
-loc = ("Databases/dataset1/Fake.csv")
- 
-wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(0)
-sheet.cell_value(0, 0)
+loc = ("Databases/Dataset1/False.csv")
+workbook = xlrd.open_workbook(loc)
+sheet = workbook.sheet_by_index(0)
+print("Finished")
  
 # Extracting number of rows
 print(sheet.nrows)
